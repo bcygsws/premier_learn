f30d3500d58c3ae1c0203101cc8e128434408cf9
bcygsws <bcygswspring168@gmail.com>
2025-07-30 04:28:08 +0800

    :memo: 01_js基础，哪些事件不会冒泡

--- 01_js/01_js基础.js
+++ 01_js/01_js基础.js
@@ -1,3 +1,20 @@
-console.log('hello world !~~········');
+/**
+ * @Desc:01-不会冒泡的事件有哪些？
+ * 5个一定不会冒泡 && 4个某些场景上会冒泡
+ *
+ * focus
+ * blur
+ * focusin: 功能类似focus，但是会在目标元素及其父元素上冒泡，这是个特例
+ * focusout：功能类似focus，但是会在目标元素及其父元素上冒泡，这是个特例
+ *
+ * load
+ * unload
+ * stop
+ *
+ * scroll：在某些浏览器上，目标元素滚动时，会触发冒泡
+ * readystatechange：当document readystate 改变时，会触发冒泡
+ *
+ *
+ * */
 
 
